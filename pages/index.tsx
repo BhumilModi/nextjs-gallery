517d7e943edb4d99e79d6e6ad86860b5015e5853
Bhumil Modi <bhumilmodi2002@gmail.com>
2022-11-15 23:00:15 +0530

    Gallery

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,13 @@ export async function getStaticProps() {
 type Photo = {
   id: number;
   href: string;
+  src: string;
   name: string;
   userName: string;
-  src: string;
 };
 
 const Gallery = ({photos}: {photos: Photo[]}) => {
+  // console.log(photos);
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:grid-gap-x-8 ">
